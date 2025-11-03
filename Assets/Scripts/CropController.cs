85653c6d7bf3b1cca891830de8461b87d902dd1f
kkorentin <korentin.kolaczek@gmail.com>
2025-11-03 15:24:55 +0100

    feat/add different crops

---
+++ Assets/Scripts/CropController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropController : MonoBehaviour
+{
+    public static CropController instance;
+    private void Awake() {
+        if(instance == null)
+        { 
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public enum CropType
+    {
+        pumpkin,
+        lettuce,
+        carrot,
+        hay,
+        potato,
+        strawberry,
+        tomato,
+        eggplant
+    }
+
+    public List<CropInfo> cropList = new List<CropInfo>();
+}
+[System.Serializable]
+public class CropInfo
+{
+    public CropController.CropType cropType;
+    public Sprite finalCrop,seedType,planted,growStage1,GrowStage2,ripe;
+
+    public int seedAmount, cropAmount;
+}
