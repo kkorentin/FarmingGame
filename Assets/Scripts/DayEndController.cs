9126344db2e9edcee0f5ad051afeb826765fec61
kkorentin <korentin.kolaczek@gmail.com>
2025-11-12 18:38:37 +0100

    feat/add music

--- Assets/Scripts/DayEndController.cs
+++ Assets/Scripts/DayEndController.cs
@@ -16,6 +16,8 @@ public class DayEndController : MonoBehaviour
         {
             dayText.text = "- Day " + TimeController.instance.currentDay +" -";
         }
+
+        AudioManager.instance.PauseMusic();
     }
 
     // Update is called once per frame
@@ -24,8 +26,8 @@ public class DayEndController : MonoBehaviour
         if(Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame)
         {
             TimeController.instance.StartDay();
-
-           SceneManager.LoadScene(wakeUpScene);
+            AudioManager.instance.ResumeMusic();
+            SceneManager.LoadScene(wakeUpScene);
         }
     }
 }
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,9 +5,16 @@ public class MainMenuController : MonoBehaviour
 {
     public string levelToStart;
 
+    private void Start()
+    {
+        AudioManager.instance.PlayTitle();
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(levelToStart);
+
+        AudioManager.instance.PlayNextBGM();
     }
 
     public void QuitGame()
